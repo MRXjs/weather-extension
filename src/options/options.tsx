77ad49ef47d88559e7694be0254facc0c84f96f0
MRX <sureshmadawa763@gmail.com>
2024-02-26 22:23:13 +0530

    webpack basic setup done

---
+++ src/options/options.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+// import "./popup.css";
+
+const test = <h1>Option Page</h1>
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+const reactRoot = createRoot(root)
+reactRoot.render(test)
